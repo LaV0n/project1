576f3f4eff546af0e52dd63bc4753cc711b01019
LaVon <leonidmedica@gmail.com>
2022-08-24 10:24:46 +0200

    finished style

--- src/components/testPage/TestPage.tsx
+++ src/components/testPage/TestPage.tsx
@@ -1,9 +1,21 @@
 import React from "react";
+import SuperInputText from "../common/c1-SuperInputText/SuperInputText";
+import SuperButton from "../common/c2-SuperButton/SuperButton";
+import SuperCheckbox from "../common/c3-SuperCheckbox/SuperCheckbox";
 
 export const TestPage = () => {
     return (
         <div>
-            Test  PAGE
+            <div>Test  PAGE</div>
+            <div>
+                <SuperInputText/>
+            </div>
+            <div>
+                <SuperButton>Button</SuperButton>
+            </div>
+            <div>
+                <SuperCheckbox/>
+            </div>
         </div>
     )
 }
