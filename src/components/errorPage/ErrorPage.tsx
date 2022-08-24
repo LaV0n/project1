2993a53fd6910c100c51b09642af07eba4815764
LaVon <leonidmedica@gmail.com>
2022-08-24 11:43:05 +0200

    homework is done

--- src/components/errorPage/ErrorPage.tsx
+++ src/components/errorPage/ErrorPage.tsx
@@ -1,9 +1,14 @@
 import React from "react";
+import styles from "./ErrorPage.module.css"
+import gif from "../../assets/image/419.gif"
 
 export const ErrorPage = () => {
     return (
-        <div>
-            ERROR -=404=- PAGE
+        <div className={styles.block}>
+            <div>
+                ERROR -=404=- PAGE
+            </div>
+            <img src={gif} alt={'0'}/>
         </div>
     )
 }
