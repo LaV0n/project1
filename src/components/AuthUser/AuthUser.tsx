cbc9c908737a5eaa02aa9c8dbecc6f225099d071
Alex Lukomsky <lukomsky.alexander@gmail.com>
2022-09-19 14:01:44 +0300

    refactor: dafault avatar

--- src/components/AuthUser/AuthUser.tsx
+++ src/components/AuthUser/AuthUser.tsx
@@ -2,7 +2,9 @@ import { NavLink } from 'react-router-dom';
 import { appPath } from '../../common/path/appPath';
 import style from './authUser.module.scss'
 export const AuthUser = () => {
+   const userAvatar = false
    const userName = 'Dream Team'
+   const defaultAvatar = "https://static.vecteezy.com/system/resources/thumbnails/008/215/293/small/funny-funky-monkey-line-pop-art-logo-colorful-design-with-dark-background-abstract-illustration-isolated-black-background-for-t-shirt-poster-clothing-merch-apparel-badge-design-vector.jpg"
    return (
       <div className={style.user}>
          <NavLink className={style.user__nickname} to={appPath.PROFILE}>
@@ -10,7 +12,7 @@ export const AuthUser = () => {
          </NavLink>
          <img
             className={style.user__avatar}
-            src="https://static.vecteezy.com/system/resources/thumbnails/008/215/293/small/funny-funky-monkey-line-pop-art-logo-colorful-design-with-dark-background-abstract-illustration-isolated-black-background-for-t-shirt-poster-clothing-merch-apparel-badge-design-vector.jpg"
+            src={userAvatar ? userAvatar : defaultAvatar}
             alt="" />
       </div>
    )
