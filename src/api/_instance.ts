2555622b0527b6874ea3e304adad384a5436908c
Alex Lukomsky <lukomsky.alexander@gmail.com>
2022-09-21 02:49:40 +0300

    feat: add hashRouter for gh pages,  add instance path

--- src/api/_instance.ts
+++ src/api/_instance.ts
@@ -4,3 +4,13 @@ export const _instance = axios.create({
    baseURL: 'http://localhost:7542/2.0/',
    withCredentials: true
 })
+
+//    'https://neko-back.herokuapp.com/2.0/'
+//    'http://localhost:7542/2.0/'
+
+
+
+
+
+//process.env.REACT_APP_BACK_URL
+//process.env.REACT_APP_LOCAL_HOST 
--- src/index.tsx
+++ src/index.tsx
@@ -3,12 +3,13 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './app/App';
 import reportWebVitals from './reportWebVitals';
-import {BrowserRouter} from "react-router-dom";
-import {Provider} from "react-redux";
-import {store} from "./app/store";
+import { BrowserRouter } from "react-router-dom";
+import { Provider } from "react-redux";
+import { store } from "./app/store";
+import { HashRouter } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
+    document.getElementById('root') as HTMLElement
 );
 root.render(
     <Provider store={store}>
@@ -18,7 +19,15 @@ root.render(
     </Provider>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+
+//for gh pages
+// root.render(
+//     <Provider store={store}>
+//         <HashRouter>
+//             <App />
+//         </HashRouter>
+//     </Provider>
+// );
+
+
 reportWebVitals();
