575b3b30f4426a4de36cb768a65a5fa9161a3d24
Alex Lukomsky <lukomsky.alexander@gmail.com>
2022-09-21 15:45:24 +0300

    feat: add path newpassword heroku

--- src/api/auth-api.ts
+++ src/api/auth-api.ts
@@ -23,7 +23,7 @@ export const authAPI = {
          from: "test-front-admin <ai73a@yandex.by>",
          message: `<div style="background-color: lime; padding: 15px">
                    password recovery link: 
-                   <a href='http://localhost:3000/newpassword/$token$'>
+                   <a href='https://lav0n.github.io/project1/#/newpassword/$token$'>
                    link</a>
                    </div>`
       })
