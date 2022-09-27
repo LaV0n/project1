dfb02d46e207f03f5c985a00a81be1d5082ed2f2
LaVon <leonidmedica@gmail.com>
2022-09-27 10:27:20 +0200

    fix: fix keys

--- src/components/RatingStars/RatingStars.tsx
+++ src/components/RatingStars/RatingStars.tsx
@@ -9,7 +9,7 @@ type RatingStarsType={
 export const RatingStars: FC<RatingStarsType> = ({stars})=>{
     return(
         <div>
-            {[0,1,2,3,4].map(s=>s<stars? <StarIcon color='warning'/>:<StarOutlineIcon color={'disabled'}/>)
+            {[0,1,2,3,4].map(s=>s<stars? <StarIcon key={s} color='warning'/>:<StarOutlineIcon key={s} color={'disabled'}/>)
             }
         </div>
     )
--- src/features/cards/Cards..tsx
+++ src/features/cards/Cards..tsx
@@ -9,18 +9,19 @@ import {CircularProgress} from "@mui/material";
 import {CustomizedSnackbars} from "../../components/CustomizedSnackbars/CustomizedSnackbars";
 import vectorIcon from "../../assets/icons/Vector 1.png"
 
-type CardsType ={
-    packId:string
+type CardsType = {
+    packId: string
 }
 
 //export const packId='632f9975ef99210257c3d00f'                                                         //plug
 export const packId = '6330327a85107f309033e65b'                                                         //plug owner
 
-export const Cards:FC<CardsType> = ({packId}) => {
+export const Cards: FC<CardsType> = ({packId}) => {
     const data = useAppSelector(state => state.cards)
     const userId = useAppSelector(state => state.auth.data._id)
-    const notice=useAppSelector(state => state.cards.notice)
+    const notice = useAppSelector(state => state.cards.notice)
     const dispatch = useAppDispatch()
+
 
     const onCloseSnackbar = () => {
         dispatch(setErrorNotice({notice: ''}))
@@ -33,13 +34,14 @@ export const Cards:FC<CardsType> = ({packId}) => {
     return (
         <div className={styles.container}>
             {data.status && <CircularProgress style={{zIndex: '3', position: 'absolute', left: '50vw', top: '50vh'}}/>}
-           <NavLink to={appPath.MAIN} className={styles.link}>                                       {/*//temp link*/}
+            <NavLink to={appPath.MAIN} className={styles.link}>                                       {/*//temp link*/}
                 <img src={vectorIcon} alt={''}/> Back to Packs list
             </NavLink>
-              <CardsTable cards={data.data.cards}
+            <CardsTable cards={data.data.cards}
                         isOwner={userId === data.data.packUserId}
                         packId={packId}
                         status={data.status}
+
             />
             cardsCount : {data.data.cardsTotalCount}
             <CustomizedSnackbars message={notice} isOpen={!!notice} onClose={onCloseSnackbar}
