22bd6365aca9a6fed1c9450764466bb09db8f140
Alex Lukomsky <lukomsky.alexander@gmail.com>
2022-09-28 13:39:28 +0300

    fix: slider

--- src/features/packs/PacksCountFilter/PacksCountFilter.tsx
+++ src/features/packs/PacksCountFilter/PacksCountFilter.tsx
@@ -7,32 +7,30 @@ import "./thumb.scss"
 export const PacksFilterCount = () => {
    const dispatch = useAppDispatch()
    const { minCardsCount, maxCardsCount } = useAppSelector(state => state.packs.data)
-   const { min, max } = useAppSelector(state => state.packs.params)
-   const minValue = min !== null ? min : minCardsCount
-   const maxValue = max !== null ? max : maxCardsCount
-   const [values, setValues] = useState({ min: minValue === maxValue ? 0 : minValue, max: maxValue })
+   const [values, setValues] = useState({ min: minCardsCount, max: maxCardsCount })
    useEffect(() => {
-      setValues({ min: minValue, max: maxValue })
-   }, [minValue, maxValue])
+      setValues({ min: minCardsCount, max: maxCardsCount })
+   }, [minCardsCount, maxCardsCount])
    const onChangeHandler = (event: Event, value: number | number[]) => {
       const valuesAsArr = value as number[]
       if (valuesAsArr[1] < 1) return
-      setValues(values => values = { min: valuesAsArr[0], max: valuesAsArr[1] })
+      setValues({ min: valuesAsArr[0], max: valuesAsArr[1] })
    }
    const setFilterChanges = () => { dispatch(setFilterValues(values)) }
    return (
       <div className={`${style.filter} packs-filter`}>
          <div className={style.filter__title}>Number of cards</div>
          <div className={style.filter__row}>
-            <> <div className={style.filter__display_value}>{values.min}</div>
-               <Slider
-                  value={[values.min, values.max]}
-                  onChange={onChangeHandler}
-                  min={minCardsCount}
-                  max={maxCardsCount}
-                  onChangeCommitted={setFilterChanges}
-               />
-            </>
+            <div className={style.filter__display_value}>{values.min}</div>
+            <Slider
+               value={[minCardsCount === maxCardsCount ? 0 : values.min, values.max]}
+               onChange={onChangeHandler}
+               min={minCardsCount === maxCardsCount ? 0 : minCardsCount}
+               max={maxCardsCount}
+               onChangeCommitted={setFilterChanges}
+               disabled={minCardsCount === maxCardsCount}
+            />
+
             <div className={style.filter__display_value}>{values.max}</div>
          </div>
       </div>
