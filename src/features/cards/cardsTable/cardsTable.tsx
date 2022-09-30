585fb0eb3322f149907d7c4629b9148c1953e5ae
Alex Lukomsky <lukomsky.alexander@gmail.com>
2022-09-30 15:27:59 +0300

    fix: unwanted rerender cards

--- src/features/cards/cardsTable/cardsTable.tsx
+++ src/features/cards/cardsTable/cardsTable.tsx
@@ -1,21 +1,21 @@
-import { Table, TableContainer  } from "@mui/material";
-import {ChangeEvent, FC, useEffect, useState} from "react";
+import { Table, TableContainer } from "@mui/material";
+import { ChangeEvent, FC, useEffect, useState } from "react";
 import React from "react";
-import { getCardsTC, setPage, setPageCount} from "../cardsReducer";
+import { getCardsTC, setPage, setPageCount } from "../cardsReducer";
 import styles from "./cardsTable.module.scss"
-import {useAppDispatch, useAppSelector} from "../../../app/store";
+import { useAppDispatch, useAppSelector } from "../../../app/store";
 import useDebounce from "../../../common/utils/hooks";
-import {CardsFooter} from "../cardsFooter/cardsFooter";
-import {TableHeader} from "./tableHeader/TableHeader";
-import {TableCardHead} from "./tableCardHead/TableCardHead";
-import {TableCardBody} from "./tableCardBody/TableCardBody";
+import { CardsFooter } from "../cardsFooter/cardsFooter";
+import { TableHeader } from "./tableHeader/TableHeader";
+import { TableCardHead } from "./tableCardHead/TableCardHead";
+import { TableCardBody } from "./tableCardBody/TableCardBody";
 
 type CardsTablePropsType = {
     isOwner: boolean
     packId: string
 }
 
-export const CardsTable: FC<CardsTablePropsType> = ({isOwner, packId}) => {
+export const CardsTable: FC<CardsTablePropsType> = ({ isOwner, packId }) => {
 
     const dispatch = useAppDispatch()
     const pageCount = useAppSelector(state => state.cards.pageCount)
@@ -38,27 +38,27 @@ export const CardsTable: FC<CardsTablePropsType> = ({isOwner, packId}) => {
 
     useEffect(() => {
         localStorage.setItem('searchItem', debouncedValue)
-        dispatch(getCardsTC({cardsPack_id: packId, cardQuestion: debouncedValue}))
-    }, [debouncedValue, pageCount, page,cardsTotalCount])
+        dispatch(getCardsTC({ cardsPack_id: packId, cardQuestion: debouncedValue }))
+    }, [debouncedValue, pageCount, page])
 
     return (
         <div className={styles.container}>
-            <TableHeader isOwner={isOwner} packId={packId}/>
+            <TableHeader isOwner={isOwner} packId={packId} />
             <div className={styles.searchBlock}>
                 <div className={styles.searchTitle}>Search</div>
                 <input type={'search'} placeholder={'Provide your text'}
-                       style={{width: '100%', height: '30px', border: '1px solid rgba(0, 0, 0, 0.1)'}}
-                       onChange={onChangeHandler} value={searchInput}/>
+                    style={{ width: '100%', height: '30px', border: '1px solid rgba(0, 0, 0, 0.1)' }}
+                    onChange={onChangeHandler} value={searchInput} />
             </div>
             <TableContainer className={styles.table}>
                 <Table aria-label="simple table">
-                    <TableCardHead packId={packId} isOwner={isOwner}/>
-                    <TableCardBody isOwner={isOwner}/>
+                    <TableCardHead packId={packId} isOwner={isOwner} />
+                    <TableCardBody isOwner={isOwner} />
                 </Table>
             </TableContainer>
             <CardsFooter page={page} cardsTotalCount={cardsTotalCount}
-                         pageCount={pageCount} onChangePageCount={onChangePageCountHandler}
-                         onChangePage={onChangePageHandler}/>
+                pageCount={pageCount} onChangePageCount={onChangePageCountHandler}
+                onChangePage={onChangePageHandler} />
         </div>
     );
 }
