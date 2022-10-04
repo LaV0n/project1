1a80093257d59610e6a3c30624dc521c081692da
Alex Lukomsky <lukomsky.alexander@gmail.com>
2022-10-04 12:01:03 +0300

    feat: browser router

--- src/index.tsx
+++ src/index.tsx
@@ -11,23 +11,23 @@ import { HashRouter } from 'react-router-dom';
 const root = ReactDOM.createRoot(
     document.getElementById('root') as HTMLElement
 );
-// root.render(
-//     <Provider store={store}>
-//         <BrowserRouter>
-//             <App />
-//         </BrowserRouter>
-//     </Provider>
-// );
+root.render(
+    <Provider store={store}>
+        <BrowserRouter>
+            <App />
+        </BrowserRouter>
+    </Provider>
+);
 
 
 //for gh pages
-root.render(
-    <Provider store={store}>
-        <HashRouter>
-            <App />
-        </HashRouter>
-    </Provider>
-);
+// root.render(
+//     <Provider store={store}>
+//         <HashRouter>
+//             <App />
+//         </HashRouter>
+//     </Provider>
+// );
 
 
 reportWebVitals();
